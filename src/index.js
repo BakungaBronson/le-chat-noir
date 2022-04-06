e782058a752e9d02d60eac57eaaf3ec9ac5ef04a
BakungaBronson <bakungabro@gmail.com>
2022-04-06 16:15:48 +0000

    Modified to render landing component

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Start from './views/Start';
+import { Landing } from './views/Landing';
+
+const state = {
+  start: false,
+}
 
 ReactDOM.render(
-  <section className='intro'>
-    <div className='row'>
-      <h1>Le Chat Noir</h1>
-      <img src="https://www.lifepng.com/wp-content/uploads/2020/11/Le-Chat-Noir-png-hd.png" width="200px" />
-    </div>
-    <div className='row'>
-      <input type='text' placeholder='Enter number of rows' />
-      <br/>
-      <button>Send</button>
-    </div>
-  </section>,
+  <Landing />,
   document.getElementById('root')
 );
 
